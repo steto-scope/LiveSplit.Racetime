580273f71bcfe38d452339eff15b2e6f73c9c553
steto-scope <error0x2b1@googlemail.com>
2020-02-04 22:50:09 +0100

    menu integration

---
+++ RacetimeAPI.cs
@@ -0,0 +1,82 @@
+﻿using LiveSplit.Options;
+using LiveSplit.Racetime.Model;
+using LiveSplit.Web;
+using LiveSplit.Web.SRL;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ 
+ Races List:
+ 
+ {
+  "count": "5552",
+  "games": [
+    {
+      "id": 1,
+      "name": "Super Mario 64",
+      "abbrev": "sm64",
+      "popularity": 974,
+      "popularityrank": 1
+    },
+*/
+
+
+namespace LiveSplit.Racetime
+{
+    public class RacetimeAPI
+    {
+        protected static readonly Uri BaseUri = new Uri("http://192.168.178.70:8000/");
+        protected static string racesEndpoint => "races/data";
+
+        public event EventHandler RacetimeRacesRefreshed;
+        private static RacetimeAPI _instance;
+        public static RacetimeAPI Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new RacetimeAPI();
+                return _instance;
+            }
+        }
+
+        public IEnumerable<Race> RacesList { get; set; }
+
+
+        protected Uri GetUri(string subUri)
+        {
+            return new Uri(BaseUri, subUri);
+        }
+
+        public void RefreshRacesListAsync()
+        {
+            Task.Factory.StartNew(() => RefreshRacesList());
+        }
+
+        protected void RefreshRacesList()
+        {
+            RacesList = GetRaces().ToArray();
+            RacetimeRacesRefreshed?.Invoke(null, new EventArgs());
+        }
+
+
+        protected IEnumerable<Race> GetRaces()
+        {
+            var races = JSON.FromUri(new Uri(BaseUri.AbsoluteUri + racesEndpoint)).races;
+            
+            foreach(var r in races)
+            {
+                yield return Race.Deserialize(r);
+            }
+            yield break;
+        }
+
+
+    }
+}
---
+++ View/ChannelForm.cs
@@ -0,0 +1,75 @@
+﻿using DarkUI.Forms;
+using LiveSplit.Racetime.Controller;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LiveSplit.Racetime.View
+{
+    public partial class ChannelForm : DarkForm
+    {
+        public RacetimeChannel Channel { get; set; }
+
+        internal ChannelForm()
+        {
+            InitializeComponent();
+        }
+
+        public ChannelForm(RacetimeChannel channel, bool alwaysOnTop = false)
+        {
+            Channel = channel;
+            Channel.ChannelJoined += Channel_ChannelJoined;
+            Channel.StateChanged += Channel_StateChanged;
+            Channel.UserListRefreshed += Channel_UserListRefreshed;
+            Channel.GoalChanged += Channel_GoalChanged;
+            Channel.ChatUpdate += Channel_ChatUpdate;
+            Channel.Disconnected += Channel_Disconnected;
+
+            InitializeComponent();
+            TopMost = alwaysOnTop;
+            Show();
+        }
+
+        private void Channel_Disconnected(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Channel_ChatUpdate(object sender, string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Channel_GoalChanged(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Channel_UserListRefreshed(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Channel_StateChanged(object sender, Model.RaceState value)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Channel_ChannelJoined(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ChannelWindow_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }       
+}
