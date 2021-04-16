7fe788be29b57ae195e1125bd07985d007a1bdac
Killklli <tvatter@hotmail.com>
2021-04-16 10:18:13 -0500

    add in auth headers

--- Controller/RacetimeChannel.cs
+++ Controller/RacetimeChannel.cs
@@ -47,7 +47,7 @@ namespace LiveSplit.Racetime.Controller
         public string Username { get; set; }
         public bool Invited = false;
         public TimeSpan Offset { get; set; }
-
+        public string Token { get; set; }
         CancellationTokenSource websocket_cts;
         CancellationTokenSource reconnect_cts;
 
@@ -230,7 +230,7 @@ namespace LiveSplit.Racetime.Controller
 
                 //opening the socket
                 ws.Options.SetRequestHeader("Authorization", $"Bearer {Authenticator.AccessToken}");
-
+                Token = Authenticator.AccessToken;
                 try
                 {
                     await ws.ConnectAsync(new Uri(FullSocketRoot + "ws/o/race/" + id), websocket_cts.Token);
--- View/ChannelForm.cs
+++ View/ChannelForm.cs
@@ -1,4 +1,5 @@
 ﻿using CefSharp;
+using CefSharp.Handler;
 using DarkUI.Forms;
 using LiveSplit.Racetime.Controller;
 using LiveSplit.Racetime.Model;
@@ -23,13 +24,14 @@ namespace LiveSplit.Racetime.View
             Text = "Connecting to " + channelId.Substring(channelId.IndexOf('/') + 1);
             Channel.Connect(channelId);
             chatBox.LifeSpanHandler = new ChatBoxLifeSpanHandler();
+            chatBox.RequestHandler = new BearerAuthRequestHandler(Channel.Token);
         }
+
 
         private void Channel_RaceChanged(object sender, EventArgs e)
         {
             Text = $"{Channel.Race.Goal} [{Channel.Race.GameName}] - {Channel.Race.ChannelName}";
             chatBox.Load(Channel.FullWebRoot + Channel.Race.Id + "/livesplit");
-
         }
 
         private void Channel_Authorized(object sender, EventArgs e)
@@ -78,5 +80,43 @@ namespace LiveSplit.Racetime.View
         public void OnBeforeClose(IWebBrowser browser) { }
         public void OnAfterCreated(IWebBrowser chromiumWebBrowser, IBrowser browser) { }
         public void OnBeforeClose(IWebBrowser chromiumWebBrowser, IBrowser browser) { }
+    }
+    class BearerAuthResourceRequestHandler : ResourceRequestHandler
+    {
+        public BearerAuthResourceRequestHandler(string token)
+        {
+            _token = token;
+        }
+
+        private string _token;
+
+        protected override CefReturnValue OnBeforeResourceLoad(IWebBrowser chromiumWebBrowser, IBrowser browser, IFrame frame, IRequest request, IRequestCallback callback)
+        {
+            if (!string.IsNullOrEmpty(_token))
+            {
+                var headers = request.Headers;
+                if (request.Url.Contains(Properties.Resources.PROTOCOL_REST + "://" + Properties.Resources.DOMAIN))
+                    headers["Authorization"] = $"Bearer {_token}";
+                request.Headers = headers;
+                return CefReturnValue.Continue;
+            }
+            else return base.OnBeforeResourceLoad(chromiumWebBrowser, browser, frame, request, callback);
+        }
+
+    }
+    class BearerAuthRequestHandler : RequestHandler
+    {
+        public BearerAuthRequestHandler(string token)
+        {
+            _token = token;
+        }
+
+        private string _token;
+
+        protected override IResourceRequestHandler GetResourceRequestHandler(IWebBrowser chromiumWebBrowser, IBrowser browser, IFrame frame, IRequest request, bool isNavigation, bool isDownload, string requestInitiator, ref bool disableDefaultHandling)
+        {
+            if (!string.IsNullOrEmpty(_token)) return new BearerAuthResourceRequestHandler(_token);
+            else return base.GetResourceRequestHandler(chromiumWebBrowser, browser, frame, request, isNavigation, isDownload, requestInitiator, ref disableDefaultHandling);
+        }
     }
 }
